b45aeeebe85627ae24cc11b16b4e649229e937a3
abishek <reddyabishek435@gmail.com>
2017-11-19 14:55:02 -0500

    Testing

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var About = (props) => {
     return (
         <div>
             <h1 className="text-center page-title">About</h1>
-           <p>Abishek Profile!</p>
+           <p>My Name is Abishek!</p>
         </div>
     );
 };
