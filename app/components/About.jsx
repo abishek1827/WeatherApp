7aa60fab4de6b127dcf286b9e245c32220e85cb8
abishek <reddyabishek435@gmail.com>
2017-11-20 16:05:43 -0500

    Added Changes for About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var About = (props) => {
     return (
         <div>
             <h1 className="text-center page-title">About</h1>
-            <h3 className="text-center">Hi, I'm Abishek. </h3>
+            <h3 className="text-center">Hi, I'm Abishek.</h3>
             <p className="text-center">This is a weather application that displays the weather of the city you want to find about.</p>
         </div>
     );
